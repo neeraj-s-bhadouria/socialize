8b9ee99a5dac19c20c9ed94a18a7ae668dbdec9f
neeraj-s-bhadouria <neerajsb28@gmail.com>
2023-09-02 16:14:28 +0530

    setting up theme for the app

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const LoginPage = () => {
     return <h1>LoginPage</h1>;
 };
--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const ProfilePage = () => {
     return <h1>ProfilePage</h1>;
 };
